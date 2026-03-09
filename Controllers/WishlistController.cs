8e22b9512b771616bc6c8b0bf2e51cc0acc36f23
Elif Avcu <elifavcuu07@gmail.com>
2026-03-09 22:57:17 +0300

    feat: create Toggle IActionResult in WishlistController

--- Controllers/WishlistController.cs
+++ Controllers/WishlistController.cs
@@ -1,8 +1,26 @@
+using System.Security.Claims;
+using liva_store.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace liva_store.Controllers;
 
+[Authorize]
 public class WishlistController : Controller
 {
-    
+    private readonly IWishlistService _wishlistService;
+
+    public WishlistController(IWishlistService wishlistService)
+    {
+        _wishlistService = wishlistService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Toggle(int productId)
+    {
+        var userId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        bool isAdded = await _wishlistService.ToggleWishlistItemAsync(userId, productId);
+
+        return Json(new { success = true, isAdded = isAdded });
+    }
 }
