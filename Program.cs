b91287dcead4341a7dc06ae3acdeb6122da0f3ac
Elif Avcu <elifavcuu07@gmail.com>
2026-03-10 18:25:13 +0300

    feat(di): register IWishlistService in Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddTransient<IEmailService, MailKitEmailService>();// SmtpEmailService sınıfındaki yöntemler eskide kaldı.
 builder.Services.AddTransient<ICartService, CartService>();
+builder.Services.AddScoped<IWishlistService, WishlistService>();
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<DataContext>(options =>
