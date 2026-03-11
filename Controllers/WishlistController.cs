9c9f1d60ded8a8fc49d3e3c6a2f012865bed63d7
Elif Avcu <elifavcuu07@gmail.com>
2026-03-11 23:03:52 +0300

    feat(WishlistController) : add Index action to display wishlist items

--- Controllers/WishlistController.cs
+++ Controllers/WishlistController.cs
@@ -1,7 +1,9 @@
 using System.Security.Claims;
+using liva_store.Data;
 using liva_store.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace liva_store.Controllers;
 
@@ -9,12 +11,20 @@ namespace liva_store.Controllers;
 public class WishlistController : Controller
 {
     private readonly IWishlistService _wishlistService;
+    private readonly DataContext _context;
 
-    public WishlistController(IWishlistService wishlistService)
+    public WishlistController(IWishlistService wishlistService, DataContext context)
     {
         _wishlistService = wishlistService;
+        _context = context;
     }
 
+    public async Task<IActionResult> Index()
+    {
+        var userId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+        var wishlistItems = await _context.WishlistItems.Include(wli => wli.Product).Where(w => w.Wishlist.UserId == userId).ToListAsync();
+        return View(wishlistItems);
+    }
     [HttpPost]
     public async Task<IActionResult> Toggle(int productId)
     {
